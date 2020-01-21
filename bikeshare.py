aa40b4dc21786dc9ee6679b0b47ba801344f108d
Naomi Umezurike <nu52808@gmail.com>
2020-01-21 02:44:29 -0500

    refactor: removed items to be returned in results for bikeshare cities.

--- bikeshare.py
+++ bikeshare.py
@@ -15,10 +15,10 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('Hello! Let\'s explore some US city bikeshare data!')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     city = input('Enter City(Chicago, New York, Washington); ')
-    
+
     while city.lower() not in ['chicago', 'new york', 'washington']:
         city = input('Please try again: ')
 
@@ -50,9 +50,9 @@ def load_data(city, month, day):
     Returns:
         df - Pandas DataFrame containing city data filtered by month and day
     """
-    month_data = {'january': 1, 'february': 2, 'march': 3, 'april': 4, 'may': 5, 'june': 6, 'july': 7, 
+    month_data = {'january': 1, 'february': 2, 'march': 3, 'april': 4, 'may': 5, 'june': 6, 'july': 7,
                   'august': 8, 'september': 9, 'october': 10, 'november': 11, 'december': 12}
-    city_data = { 'chicago': 'chicago.csv', 'new york': 'new_york_city.csv', 
+    city_data = { 'chicago': 'chicago.csv', 'new york': 'new_york_city.csv',
                  'washington': 'washington.csv' }
     weekday_data = {'monday': 0, 'tuesday': 1, 'wednesday': 2, 'thursday': 3, 'friday': 4,
                     'saturday': 5, 'sunday': 6}
@@ -72,14 +72,14 @@ def time_stats(df):
     start_time = time.time()
 
     # TO DO: display the most common month
-    month_data = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June', 7: 'July', 
+    month_data = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June', 7: 'July',
                   8: 'August', 9: 'September', 10: 'October', 11: 'November', 12: 'December'}
     month = pd.to_datetime(df['Start Time']).apply(lambda x: x.month).value_counts().idxmax()
     print('Month: ', month_data[month])
 
 
     # TO DO: display the most common day of week
-    weekday_data = {0: 'Monday', 1: 'Tuesday', 2: 'Wednesday', 3: 'Thursday', 4: 'Friday', 
+    weekday_data = {0: 'Monday', 1: 'Tuesday', 2: 'Wednesday', 3: 'Thursday', 4: 'Friday',
                     5: 'Saturday', 6: 'Sunday'}
     weekday = pd.to_datetime(df['Start Time']).apply(lambda x: x.weekday()).value_counts().idxmax()
     print('Weekday: ', weekday_data[weekday])
